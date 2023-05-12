69977c49b8a03b8a9d5d0f6014c0db0e4cb541e6
Dillbotz17 <132948253+Dillbotz17@users.noreply.github.com>
2023-05-12 17:05:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6285822347348'],
-  ['6285822347348'],
-  ['6285822347348', 'Owner', true]
+  ['6288271005021'],
+  ['6288271005021'],
+  ['6288271005021', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -41,7 +41,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'Sticker By'
-  var sticker_author = 'ArullOfc'
+  var sticker_author = 'Dillofc'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© ArullBotzMD'
+global.wm = '© DillBotzMD'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
